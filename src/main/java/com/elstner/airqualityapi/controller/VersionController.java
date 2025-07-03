ad34e14b80d19051318f3985548ffb07d47867fb
Christopher Elstner <72193429+ce451@users.noreply.github.com>
2025-07-03 07:12:14 +0200

    version from properties

--- src/main/java/com/elstner/airqualityapi/controller/VersionController.java
+++ src/main/java/com/elstner/airqualityapi/controller/VersionController.java
@@ -1,12 +1,17 @@
 package com.elstner.airqualityapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersionController {
+    @Value("${application.version}")
+    private  String version;
+
     @GetMapping("/version")
     public String getVersion() {
-        return "1.0.0";
+        // get settings from application.properties
+        return version;
     }
 }
