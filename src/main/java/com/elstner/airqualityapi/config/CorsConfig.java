3cadd76667a40e7ed53d07c5d12b99887801b0dd
Christopher Elstner <72193429+ce451@users.noreply.github.com>
2025-07-09 13:50:44 +0200

    CORS

---
+++ src/main/java/com/elstner/airqualityapi/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.elstner.airqualityapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
