ac1b42f1b3c66bc2acbcd3a4de63d1bb982095e2
Christopher Elstner <72193429+ce451@users.noreply.github.com>
2025-08-05 15:29:00 +0200

    refactored station groups; V0.4.1

--- src/main/java/com/elstner/airqualityapi/dto/StationGroupDto.java
+++ src/main/java/com/elstner/airqualityapi/dto/StationGroupDto.java
@@ -1,9 +1,22 @@
 package com.elstner.airqualityapi.dto;
 
 public class StationGroupDto {
+    private Long id;
     private String displayName;
     private Integer displayOrder;
     private Boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
 
     public String getDisplayName() {
         return displayName;
