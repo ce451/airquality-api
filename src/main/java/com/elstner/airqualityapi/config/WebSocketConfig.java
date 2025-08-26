b287a5823a690928d6cb181c4558617f58b0485c
ce <c.elstner@quentis.at>
2025-08-26 07:11:00 +0200

    websocket cors

--- src/main/java/com/elstner/airqualityapi/config/WebSocketConfig.java
+++ src/main/java/com/elstner/airqualityapi/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*") // Allow all origins for simplicity; adjust as needed
+                .setAllowedOriginPatterns("*") // Allow all origins for simplicity; adjust as needed
                 .withSockJS(); // Enable SockJS fallback options
     }
 
